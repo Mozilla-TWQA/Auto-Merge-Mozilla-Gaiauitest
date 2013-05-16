6958e0fb5da1d0a56b240532ced3c2938f028cfa
Walter Chen <wachen@mozilla.com>
2013-05-16 17:20:24 +0800

    modified to auto update tw-modified, too.

--- update_twqa_gaiauitests.sh
+++ update_twqa_gaiauitests.sh
@@ -11,14 +11,32 @@ echo "Started to synchronize TW-QA gaia-ui-tests"
 
 date
 
+# see if the folder exists
 test -d ${1}
 if [ $? == 1 ]; then
     echo "You haven't clone it yet. Now doing clone"
     git clone https://github.com/Mozilla-TWQA/gaia-ui-tests.git ${1}
 fi
 
+# update master branch
 cd ${1}
+if [ $(git remote show | grep mozilla-gaiauitests -c) == 1 ]; then
+    git remote rm mozilla-gaiauitests
+fi
+git remote add mozilla-gaiauitests https://github.com/mozilla/gaia-ui-tests.git
+git checkout master
 git pull --rebase mozilla-gaiauitests master
+git remote set-url origin git@github.com:Mozilla-TWQA/gaia-ui-tests.git
+git push
+
+# update tw-modified branch
+if [ $(git branch | grep tw-modified -c) == 1 ]; then
+    git branch -D tw-modified
+fi
+git branch tw-modified
+git checkout tw-modified
+git pull origin tw-modified
+git pull --rebase origin master
 git remote set-url origin git@github.com:Mozilla-TWQA/gaia-ui-tests.git
 git push
 
